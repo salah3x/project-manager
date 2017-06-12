d136ad228b0a7f6e225c5df007251b474554af02
salah3x <salah.loukili@gmail.com>
2017-06-12 20:51:35 +0000

    Moving user sing in/up controllers to IndexController

--- src/main/java/com/salah/projectmanager/web/IndexController.java
+++ src/main/java/com/salah/projectmanager/web/IndexController.java
@@ -5,12 +5,16 @@ import com.salah.projectmanager.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 /**
@@ -22,6 +26,9 @@ public class IndexController {
 
     @Autowired
     private GuestService guestService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
@@ -51,6 +58,36 @@ public class IndexController {
         System.out.println(m);
         guestService.sendMessage(m);
         return "redirect:/?success";
+    }
+
+    @RequestMapping(value = "signin", method = RequestMethod.GET)
+    public String signinGet(Model model) {
+        model.addAttribute("title", "Sign in");
+        return "signin";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
+    public String signupGet(Model model) {
+        model.addAttribute("title", "Sign up");
+        model.addAttribute("user", new com.salah.projectmanager.domain.User());
+        return "signup";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String signupPost(Model model, @ModelAttribute @Valid com.salah.projectmanager.domain.User user, Errors errors) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Sign up");
+            model.addAttribute("error", "error");
+            return "signup";
+        }
+        if(guestService.signIn(user.getUsername()) != null) {
+            model.addAttribute("double", "Username already exist");
+            return "signup";
+        }
+        user.setAvatar("img_avatar2.png");
+        user.setCryptPassword(passwordEncoder.encode(user.getPassword()));
+        guestService.signUp(user);
+        return "redirect:/signup?success";
     }
 
 }
